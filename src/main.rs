b2246eaf8ee0a25fe22606aeff4719c0695809a3
Bente Van Eeckhoudt <bente.van.eeckhoudt@hotmail.com>
2024-01-02 23:38:07 +0100

    Removed dialoguer to make user type his menu choices, this is more fitting for a type trainer

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,69 @@
-use dialoguer::Select;
-use std::{fs, process::exit};
+use std::{fs, io, process::exit};
 
 fn main() {
+    println!("Welcome, to type trainer, you can start your training by typing 'help'");
     loop {
         menu();
     }
 }
 
 fn menu() {
-    let options = vec!["quicktype", "copy", "[x] quit"];
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut buffer).expect("Error reading line");
+    let mut input = buffer.as_str().trim().split(" ");
 
-    let selection = Select::new()
-        .with_prompt("What do you want to do?")
-        .items(&options)
-        .interact()
-        .unwrap();
-    println!("");
+    match input.next().unwrap() {
+        "quit" => exit(0),
+        "help" => print_help_message(),
+        "quicktype" => {
+            let dir =
+                fs::read_dir("./exercises/quicktype").expect("Error: exercises folder not found");
+            let filter = dir.filter(|entry| {
+                entry.as_ref().is_ok_and(|file| {
+                    file.file_type().unwrap().is_file()
+                        && file
+                            .file_name()
+                            .to_str()
+                            .unwrap()
+                            .split_terminator(".")
+                            .last()
+                            .unwrap()
+                            == "txt"
+                })
+            });
+            let file_names: Vec<_> = filter
+                .map(|file| file.unwrap().file_name().to_str().unwrap().to_owned())
+                .collect();
 
-    if selection == options.len() - 1 {
-        exit(0);
-    }    
+            if let Some(file_name) = input.next() {
+                if file_names.contains(&file_name.to_string()) {
+                    // start exercise
+                    println!("Starting the quicktype exercise for {}", file_name);
+                } else {
+                    println!("File not found");
+                }
+            } else {
+                println!("Quicktype exercise options:");
+                for file_name in file_names {
+                    println!("{}", file_name);
+                }
+                println!("You can add other exercises by adding ")
+            }
+        }
 
-    // This is the base folder that should contain another folder for each exercise
-    let folder_path = String::from("./exercises/");
-
-    let files =
-        fs::read_dir(folder_path + options[selection]).expect("Error: exercises folder not found");
-    let mut files: Vec<String> = files
-        .map(|file| file.unwrap().file_name().to_str().unwrap().to_owned())
-        .collect();
-
-    if files.is_empty() {
-        println!("No exercises found in the {} folder!", options[selection]);
-        println!("You can add exercises by adding text files to the folder");
-        println!("");
-        // This only works for Linux
-        std::process::Command::new("clear").status().unwrap();
-        return;
+        "copy" => {}
+        _ => println!("Invalid option, type 'help' to see the options"),
     }
+}
 
-    files.push(String::from("[<-] back"));
-
-    let selection = Select::new()
-        .with_prompt("Choose an exercise:")
-        .items(&files)
-        .interact()
-        .unwrap();
-    println!("");
-
-    if selection == options.len() - 1 {
-        // This only works for Linux
-        std::process::Command::new("clear").status().unwrap();
-        return;
-    }
-
-    println!("You chose: {}", files[selection]);
+fn print_help_message() {
+    println!("There are two exercises:");
+    println!("\tquicktype: wait for a word to appear and type it as fast as possible, the test ends when you make a mistake");
+    println!("\tcopy: copy a text, when a mistake is made, you have to start the whole word again");
+    println!("Type one of the following options:");
+    println!("\t'exercise_name' to view the valid file names for the exercise");
+    println!("\t'exercise_name file_name.txt' to start an exercise");
+    println!("\t'quit' to quit the program");
+    println!("\t'help' to view this message again");
 }
