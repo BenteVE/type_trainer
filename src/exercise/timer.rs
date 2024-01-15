bd970e86572a6f35a8707594f08ed31754859038
Bente Van Eeckhoudt <bente.van.eeckhoudt@hotmail.com>
2024-01-15 22:55:22 +0100

    Changed timer to work before and after exercise

--- src/exercise/timer.rs
+++ src/exercise/timer.rs
@@ -24,39 +24,37 @@ impl Timer {
         self.end = Some(Instant::now());
     }
 
-    pub fn get_total(&self) -> Duration {
-        self.end.unwrap().duration_since(self.start.unwrap())
+    pub fn get_time(&self) -> Duration {
+        if let Some(start) = self.start {
+            if let Some(end) = self.end {
+                end.duration_since(start)
+            } else {
+                Instant::now().duration_since(start)
+            }
+        } else {
+            Duration::new(0, 0)
+        }
     }
 
     pub fn timer_expired(&self) -> bool {
         if let Some(duration) = self.duration {
-            self.start
-                .expect("The timer should be started at the start of the exercise")
-                .elapsed()
-                >= duration
+            self.get_time() >= duration
         } else {
             false
         }
     }
 
     pub fn ratio(&self) -> f64 {
-        let start = self
-            .start
-            .expect("The timer should be started at the start of the exercise");
         if let Some(duration) = self.duration {
-            return start.elapsed().as_secs_f64() / duration.as_secs_f64();
+            return self.get_time().as_secs_f64() / duration.as_secs_f64();
         } else {
-            return start.elapsed().as_secs_f64() % 60 as f64 / 60 as f64;
+            return self.get_time().as_secs_f64() % 60 as f64 / 60 as f64;
         }
     }
 
     /// Get the current time of the timer in String format = XX:XX
-    pub fn get_time(&self) -> String {
-        let mut seconds = self
-            .start
-            .expect("The timer should be started at the start of the exercise")
-            .elapsed()
-            .as_secs();
+    pub fn get_time_label(&self) -> String {
+        let mut seconds = self.get_time().as_secs();
         let minutes = seconds / 60;
         seconds %= 60;
 
@@ -77,7 +75,7 @@ impl Serialize for Timer {
         S: Serializer,
     {
         let mut state = serializer.serialize_struct("Timer", 1)?;
-        state.serialize_field("total_duration", &self.get_total().as_secs())?;
+        state.serialize_field("total_duration", &self.get_time().as_secs())?;
         state.end()
     }
 }
